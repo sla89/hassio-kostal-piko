45df863695b148819d915fd451f7dc04a979870f
sla89 <skullsplitter@die-optimisten.net>
2022-07-06 22:19:22 +0200

    Improve exception handling.

--- custom_components/kostal_piko/helper.py
+++ custom_components/kostal_piko/helper.py
@@ -1,4 +1,3 @@
-import json
 from numbers import Number
 import requests
 
@@ -10,9 +9,10 @@ class KostalPikoClient:
 
     def get_data(self, dxs_id: Number):
         response = requests.get(url=self._url + str(dxs_id), timeout=10)
-        data = json.loads(response.text)
 
         try:
+            data = response.json()
+
             for entry in data['dxsEntries']:
                 if entry['dxsId'] == dxs_id:
                     return entry['value']
@@ -20,5 +20,10 @@ class KostalPikoClient:
             raise Exception(
                 f'Kostal response does not match expected format for dxsId {dxs_id}. Got error {repr(e)} for response {response.text}'
             )
+        except requests.exceptions.JSONDecodeError as e:
+            raise Exception(
+                f'Kostal response has invalid format. Response was {response.text}: {repr(e)}'
+            )
 
-        raise Exception(f'Kostal response did not contain dxs_id {dxs_id}: {response.text}')
+        raise Exception(
+            f'Kostal response did not contain dxs_id {dxs_id}: {response.text}')
